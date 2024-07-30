5d4c3d780d36c211cb5f457c5c4c34ec5d420e7e
Deborah Baliellas <dbaliellas@gmail.com>
2024-07-29 21:22:22 -0300

    primeiro comit

---
+++ api-rest-dio/src/test/java/com/deborah/api_rest_dio/ApiRestDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deborah.api_rest_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
